34ac4881473f7e38975efc1767a946455682fac6
MohamadAlaskari <m_alskari96@hotmail.com>
2023-03-19 20:34:59 +0100

    components created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import './App.css'
-import Header from './components/Header/Header'
-import Container from './components/Container/Container'
-import Footer from './components/Footer/Footer'
+import { Header, Container, Footer } from './components/index'
 
 
 
