3d8f718411a840225b50b9881103c4400506f52f
MohamadAlaskari <m_alskari96@hotmail.com>
2023-03-20 15:02:40 +0100

    react icons

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,31 +1,32 @@
 import './Card.css'
+import { BsDownload, BsFillStarFill } from "react-icons/bs";
 
 
 const Card = (props) => {
     return (
-            <div className="most-popular-item">
-                <div className="card-wrapper">
-                    <img
-                        className="most-popular-item-img"
-                        src={props.img}
-                        alt="img_popular_01"
-                    />
-                    <div className="most-popular-item-content">
-                        <h4 className="most-popular-item-title">
-                            {props.title} <br />
-                            <span>{props.category}</span>
-                        </h4>
-                        <ul>
-                            <li>
-                                <span>{props.star}</span>
-                            </li>
-                            <li>
-                                <span>{props.download}</span>
-                            </li>
-                        </ul>
-                    </div>
+        <div className="most-popular-item">
+            <div className="card-wrapper">
+                <img
+                    className="most-popular-item-img"
+                    src={props.img}
+                    alt="img_popular_01"
+                />
+                <div className="most-popular-item-content">
+                    <h4 className="most-popular-item-title">
+                        {props.title} <br />
+                        <span>{props.category}</span>
+                    </h4>
+                    <ul>
+                        <li>
+                            <BsFillStarFill className='most-popular-item-icon star' />  <span>{props.star}</span>
+                        </li>
+                        <li>
+                            <BsDownload className='most-popular-item-icon download' /><span>{props.download}</span>
+                        </li>
+                    </ul>
                 </div>
             </div>
+        </div>
     )
 }
 
--- src/sections/MostPopular/MostPopular.jsx
+++ src/sections/MostPopular/MostPopular.jsx
@@ -8,6 +8,7 @@ import popular_05 from "../../assets/images/popular_05.jpg"
 import popular_06 from "../../assets/images/popular_06.jpg"
 import popular_07 from "../../assets/images/popular_07.jpg"
 import popular_08 from "../../assets/images/popular_08.jpg"
+//import icons
 
 
 
@@ -21,14 +22,14 @@ const MostPopular = () => {
             </div>
             <div className="most-popular-items">
                 <Card img={popular_01} title="Fortnite" category="Sandbox" star="4.5" download="1M" />
-                <Card img={popular_02} title="Dota2" category="StreamX" star="4" download="2.3M" />
+                <Card img={popular_02} title="Dota2" category="StreamX" star="4.7" download="2.3M" />
                 <Card img={popular_03} title="Pubge" category="Battle s" star="4.3" download="3.4M" />
-                <Card img={popular_04} title="Pubge mobile" category="Battle" star="4.3" download="3.4M" />
-                <Card img={popular_05} title="Pubge mobile" category="Battle" star="4.3" download="3.4M" />
-                <Card img={popular_06} title="Pubge mobile" category="Battle" star="4.3" download="3.4M" />
-                <Card img={popular_07} title="Pubge mobile" category="Battle" star="4.3" download="3.4M" />
-                <Card img={popular_08} title="Pubge mobile" category="Battle" star="4.3" download="3.4M" />
-               
+                <Card img={popular_04} title="Wareface" category="Max 3D" star="4.8" download="2.3M" />
+                <Card img={popular_05} title="Eagles Fly" category="Matrix Games" star="3.2" download="1.4M" />
+                <Card img={popular_06} title="Gamezer" category="Battle" star="4.3" download="3.4M" />
+                <Card img={popular_07} title="Pug G" category="Battle Royals" star="3.1" download="2.2M" />
+                <Card img={popular_08} title="Mini Craft" category="Legendary" star="4.8" download="2M" />
+
 
 
 
